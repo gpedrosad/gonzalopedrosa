ce1b8c4cc2977959e9cf02b03d887843b0406efb
gpedrosad <gpedrosa@uade.edu.ar>
2026-01-17 20:14:42 -0300

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { GoogleTagManager } from "@next/third-parties/google";
 import Script from "next/script";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -195,6 +196,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         <GoogleTagManager gtmId="GTM-N3DXMKTR" />
+        <Analytics />
       </body>
     </html>
   );
